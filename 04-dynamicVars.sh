d0fa553a9800b457bbc9d497327ca968a1d57517
CodingManoj <mvp2612@hotmail.com>
2023-06-02 21:31:57 -0400

    Auto Push

--- 04-dynamicVars.sh
+++ 04-dynamicVars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
-TODAYS_DATE="03JUNE2023"
+# TODAYS_DATE="03JUNE2023"                             # HardCoding a value makes it consistent
+TODAYS_DATE=$(date +%F)                                   # Always enclose expresssions in paranthesis
 echo "Good Morning and todays date is $TODAYS_DATE"
