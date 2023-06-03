0b828aeec072a260bdc9d14801cd21b3402d1500
CodingManoj <mvp2612@hotmail.com>
2023-06-02 22:07:22 -0400

    Auto Push

--- 08-functions.sh
+++ 08-functions.sh
@@ -20,6 +20,7 @@ sample() {
 status() {                                                     
     echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
     echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
+    echo -e "Load Average on the system from last 1 minute is $(uptime | awk -F , '{print  $3}' | awk -F : '{print $2}')"
 }
 
 # This is how you can call a function 
