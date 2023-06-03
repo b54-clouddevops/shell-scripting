c4bacef8d3e890f12a56fc8376ba7f78bea995c5
CodingManoj <mvp2612@hotmail.com>
2023-06-02 21:58:42 -0400

    Auto Push

---
+++ 08-functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# What is a function ? 
+# Function is to capture the repititive block and can be used on need basics.
+
+# When to use funcions ?
+# Whenever you want to use some block of code n number of times, rather writing n number of times, you can declare it as a function and can call it on need basis
+
+# Declaring a function 
+sample() {
+    echo "I am a sample function with name sample"
+    echo "I am executing the sample function"
+    echo "Sample function is completed"
+}
+
+# This is how you can call a function 
+
+sample 
