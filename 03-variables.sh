8b77cbe6fbaf6a6476bd6025291182556711f6d7
CodingManoj <mvp2612@hotmail.com>
2023-06-02 21:23:24 -0400

    Auto Push

--- 03-variables.sh
+++ 03-variables.sh
@@ -20,9 +20,9 @@ c=30
 # Syntax : $variableName : $ is going to print the value of the variable
 echo $a 
 echo -e "Value of the variable a is \e[32m; $a \e[0m"
-echo -e "Value of the variavle a is \e[32m; $a \e[0m"
-echo -e "Value of the variable a is \e[32m; $a \e[0m"
-echo -e "Value of the variable d is \e[32m; $a \e[0m"
+echo -e "Value of the variavle b is \e[32m; $b \e[0m"
+echo -e "Value of the variable c is \e[32m; $c \e[0m"
+echo -e "Value of the variable d is \e[32m; $d \e[0m"
 # If you try to print a variable that is not declared, bash will consider that as a null rather an error 
 
 # rm -rf /data/${FOLDER_TO_DELETE}/ => rm -rf /data
