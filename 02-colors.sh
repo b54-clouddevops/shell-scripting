2f83eba8bcf570af36a8ecda432108cf4680ff87
CodingManoj <mvp2612@hotmail.com>
2023-06-02 21:11:23 -0400

    Auto Push

--- 02-colors.sh
+++ 02-colors.sh
@@ -22,12 +22,13 @@
  #      echo -e "\e[COL-CODEm  Your Message To Be Printed \e[0m"
  #      echo -e "\e[32m I am printing Green Color \e[0m"
 
-echo -e "\e[32m I am printing Green Color \e[0m"
-echo -e "\e[33m I am printing Yellow Color \e[0m"
-echo -e "\e[34m I am printing Blue Color \e[0m"
+echo -e "\e[32m I am printing Green Color   \e[0m"
+echo -e "\e[33m I am printing Yellow Color  \e[0m"
+echo -e "\e[34m I am printing Blue Color    \e[0m"
 echo -e "\e[35m I am printing magenta Color \e[0m"
-echo -e "\e[36m I am printing Cyan Color \e[0m"
+echo -e "\e[36m I am printing Cyan Color    \e[0m"
 
 # Printing Background Color 
-
 echo -e "\e[43;32m I am printing Green Color with Yello Background \e[0m"
+
+
---
+++ 03-variables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+# What is a variable ? Variable is ised to store and pass values 
+# a=10
+# name=Verma
+# batch=54
+# topic=shellScripting
+
+# In bash there is no concept of Data Types : By default everything is a string
+# int a = 10 
+# string b = Manu 
+# float c = 0.15
+
+# This is how we can declare variables in Bash 
+a=10 
+b=20
+c=30 
+
+
+# Syntax : $variableName : $ is going to print the value of the variable
+echo $a 
+echo Value of the variable a is $a
