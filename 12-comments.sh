7d67257f8210c2e46bfe2cef2653d1aa9718c3f1
CodingManoj <mvp2612@hotmail.com>
2023-06-04 21:27:09 -0400

    Auto Push

--- 12-comments.sh
+++ 12-comments.sh
@@ -7,6 +7,8 @@
 
 echo "This is to demonstrate the usage of single line and multi line comments"
 
+<<COMMENT
 a=10 
 echo $a
-echo "In this infinite world, we are just star dust"
+echo "In this infinite world we are just star dust"
+COMMENT
