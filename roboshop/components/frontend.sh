1306f67d4c19d2c8c24bc71a06e65a24104f3fcb
CodingManoj <mvp2612@hotmail.com>
2023-06-05 20:55:05 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,13 +1,16 @@
 #!/bin/bash 
 
+COMPONENT=frontend
+
 ID=$(id -u)
+
 if [ $ID -ne 0 ] ; then 
-    echo -e "\e[31m This script is expected to be run by a root user or with a sudo provilege \e[0m"
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
     exit 1
 fi 
 
 echo "Installing Nginx :"
-yum install nginx -y 
+yum install nginx -y  &>> "/tmp/${COMPONENT}.log"
 
 # It's always a great idea to perform validation before you get an exception.
 # If the script is executed as a root user or as a sudo user, then it has to proceed.
