689ad9092b9fe3786f1cd39ded4f3104c316a9e9
CodingManoj <mvp2612@hotmail.com>
2023-06-05 21:51:50 -0400

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -24,3 +24,24 @@ echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m **********
 echo -n  "Configuring the $COMPONENT repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
 stat $?
+
+echo -n "Installing NodeJS :"
+yum install nodejs -y   &>> $LOGFILE 
+stat $? 
+
+echo -n "Creating the Service Account :"
+useradd roboshop 
+
+
+# 1. Let's now set up the catalogue application.
+
+# As part of operating system standards, we run all the applications and databases as a normal user but not with root user.
+
+# So to run the catalogue service we choose to run as a normal user and that user name should be more relevant to the project. Hence we will use `roboshop` as the username to run the service.
+
+# ```bash
+# # useradd roboshop
+
+# ```
+
+# 1. So let's switch to the `roboshop` user and run the following commands.
