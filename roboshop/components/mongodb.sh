1d46966c4b54f36915d076b41184ca4ad27baec8
CodingManoj <mvp2612@hotmail.com>
2023-06-05 21:21:38 -0400

    Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,44 @@
 #!/bin/bash 
 
-echo "I am mongodb"
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi 
+}
+
+echo -n  "Configuring the $COMPONENT repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $? 
+
+
+
+# 1. Setup MongoDB repos.
+
+# ```bash
+# # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# ```
+
+# 1. Install Mongo & Start Service.
+
+# ```
+# 1. Setup MongoDB repos.
+
+# ```bash
+# # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# ```
+
+# 1. Install Mongo & Start Service.
+# ```
