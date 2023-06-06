143c9f305bc6eac229e8a1e586a65e1ed1dc2f51
CodingManoj <mvp2612@hotmail.com>
2023-06-05 20:52:20 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,17 @@
 #!/bin/bash 
 
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo provilege \e[0m"
+    exit 1
+fi 
+
 echo "Installing Nginx :"
 yum install nginx -y 
+
+# It's always a great idea to perform validation before you get an exception.
+# If the script is executed as a root user or as a sudo user, then it has to proceed.
+# If not , I was to exit the script with some nice message.
 
 # The frontend is the service in RobotShop to serve the web content over Nginx.
 
