54bca2ef2efa7fd0141d6cc0b6b828fed99fce1b
CodingManoj <mvp2612@hotmail.com>
2023-06-05 21:49:12 -0400

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,26 @@
 #!/bin/bash 
 
-echo "I am catalogue "
+COMPONENT=catalogue
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi 
+}
+
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -n  "Configuring the $COMPONENT repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+stat $?
