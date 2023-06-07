c70265ba1a1d9dc174e2ddfb5c3a7a4e655d5fd7
CodingManoj <mvp2612@hotmail.com>
2023-06-06 21:03:04 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -42,9 +42,11 @@ rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $? 
 
+echo -n "Updating the Backend component reveseproxy details : "
 for component in catalogue ; do
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
 done 
+stat $? 
 
 echo -n "Starting $COMPONENT service: "
 systemctl daemon-reload &>> $LOGFILE
