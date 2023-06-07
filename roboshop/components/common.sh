b24ceb37ff094041638be9a5d7cf6673f170253b
CodingManoj <mvp2612@hotmail.com>
2023-06-06 21:08:01 -0400

    Auto Push

---
+++ roboshop/components/common.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+LOGFILE="/tmp/${COMPONENT}.log"
+APPUSER="roboshop"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then 
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi 
+}
