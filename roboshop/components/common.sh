ecd5e3d6f04f6a50478175842c15acade3a5e205
CodingManoj <mvp2612@hotmail.com>
2023-06-06 21:46:04 -0400

    Auto Push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -19,3 +19,78 @@ stat() {
     fi 
 }
 
+CREATE_USER() {
+
+    id $APPUSER &>> $LOGFILE
+    if [ $? -ne 0 ] ; then 
+        echo -n "Creating the Service Account :"
+        useradd $APPUSER  &>> $LOGFILE
+        stat $?
+    fi 
+
+}
+
+DOWNLOAD_AND_EXTRACT() {
+
+    echo -n "Downloading the $COMPONENT component :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+    stat $? 
+
+    echo -n "Copying the $COMPONENT to $APPUSER home directory :"
+    cd /home/${APPUSER}/
+    rm -rf  ${COMPONENT}  &>> $LOGFILE
+    unzip -o /tmp/${COMPONENT}.zip  &>> $LOGFILE
+    stat $?
+
+    echo -n "Modifying the ownership :"
+    mv $COMPONENT-main/ $COMPONENT
+    chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
+    stat $?
+
+}
+
+NPM_INSTALL() {
+
+    echo -n "Generating npm $COMPONENT artifacts :"
+    cd /home/${APPUSER}/${COMPONENT}/
+    npm install  &>> $LOGFILE
+    stat $?
+
+}
+
+CONFIGURE_SVC() {
+
+    echo -n "Updating the $COMPONENT systemd file :"
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
+    mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $? 
+
+    echo -n "Starting ${COMPONENT} service :"
+    systemctl daemon-reload   &>> $LOGFILE
+    systemctl enable $COMPONENT  &>> $LOGFILE
+    systemctl restart $COMPONENT &>> $LOGFILE
+    stat $? 
+
+    echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
+
+}
+
+NODEJS() {
+
+    echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+    echo -n  "Configuring the $COMPONENT repo :"
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+    stat $?
+
+    echo -n "Installing NodeJS :"
+    yum install nodejs -y   &>> $LOGFILE 
+    stat $?
+
+    CREATE_USER                 # calling Create_user function to create the roboshop user account
+
+    DOWNLOAD_AND_EXTRACT        # calling DOWNLOAD_AND_EXTRACT  function download the content
+
+    NPM_INSTALL                 # Creates artifact 
+
+}
