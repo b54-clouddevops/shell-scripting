6ac5fb93838cae77ea299998d3cb625489c84ddd
CodingManoj <mvp2612@hotmail.com>
2023-06-07 21:53:27 -0400

    Auto Push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -61,7 +61,7 @@ NPM_INSTALL() {
 CONFIGURE_SVC() {
 
     echo -n "Updating the $COMPONENT systemd file :"
-    sed -i -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
+    sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service  
     mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
     stat $? 
 
@@ -136,5 +136,13 @@ PYTHON() {
     cd /home/${APPUSER}/${COMPONENT}/
     pip3 install -r requirements.txt    &>> $LOGFILE 
     stat $?
-  
+
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop) 
+
+    echo -n "Updating the uid and gid in the $COMPONENT.ini file"
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini
+    
+    CONFIGURE_SVC
+
 }
