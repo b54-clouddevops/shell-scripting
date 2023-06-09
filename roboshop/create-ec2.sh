83bd30318b6731c459c86b32ff3ce063bc1325c9
CodingManoj <mvp2612@hotmail.com>
2023-06-08 21:36:14 -0400

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,4 +4,6 @@
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
 
-echo "AMI ID is $AMI_ID"
+echo -e "AMI ID used to launch the EC2 is \e[32m $AMI_ID \e[0m"
+
+
