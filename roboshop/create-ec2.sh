e386eeb9e8acca966e9944222d0a24e74d935397
CodingManoj <mvp2612@hotmail.com>
2023-06-09 20:54:22 -0400

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -10,7 +10,7 @@ SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-al
 echo -e "AMI ID used to launch the EC2 is \e[35m $AMI_ID \e[0m"
 echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 
-echo -e "**** Launching Server **** "
+echo -e "\e[36m **** Launching Server **** \e[0m"
 
 IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
