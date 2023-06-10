96b95fd682fe732fee566b6e63ef94133e040e08
CodingManoj <mvp2612@hotmail.com>
2023-06-09 21:07:56 -0400

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -3,6 +3,7 @@
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
 COMPONENT=$1
+HOSTEDZONEID="Z00285561194P4T83R9MO"
 
 if [ -z "$1" ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m"
@@ -20,7 +21,14 @@ echo -e "\e[36m **** Launching Server **** \e[0m"
 
 IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
+echo -e "\e[36m **** Launching $COMPONENT Server is completed **** \e[0m"
+
 echo -e "Private IP Address of $COMPONENT is \e[35m $IPADDRESS \e[0m"
+
+echo -n "\e[36m **** Creating DNS Record for the $COMPONENT : **** \e[0m"
+sed -e "s/COMPONENT/${COMPONENT}/"  -e "s/IPADDRESS/${IPADDRESS}" roboshop/route53.json > /tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json
+
          
         
         
